04a9e2343b80ade58ae7fda9d5cf77d46dbf6658
cocoakaike <akaike@cocoa.ne.jp>
2022-12-26 17:30:24 +0900

    In tablet mode, 100 ms was not enough

--- app/src/main/java/com/snaggly/ksw_toolkit/core/service/helper/CoreServiceClient.kt
+++ app/src/main/java/com/snaggly/ksw_toolkit/core/service/helper/CoreServiceClient.kt
@@ -29,7 +29,7 @@ class CoreServiceClient {
         }
 
         override fun onServiceDisconnected(name: ComponentName?) {
-            Thread.sleep(100)    // Prevent crashes during service outages
+            Thread.sleep(200)    // Prevent crashes during service outages
             coreService = null
         }
     }
